1e000445161002e0b6d95a1ae5d55797e011258f
Juhoedvard <j.puumalainen95@gmail.com>
2023-08-02 19:01:27 +0300

    bug fixes

--- src/pages/Item/[id]/edit.tsx
+++ src/pages/Item/[id]/edit.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 
@@ -33,25 +34,25 @@ const EditItem: NextPage = () => {
         fileUrl: string;
         fileKey: string;
       }[]>([]);
-    const {data: Item, isLoading} = api.item.getOne.useQuery({
+    const {data: post, isLoading} = api.item.getOne.useQuery({
         id: router.query.id as string
     })
-    (Item)
+
     const {mutate, isLoading : loadingItem} = api.item.updatePost.useMutation({
         onSuccess: () => {
             void ctx.item.getOne.invalidate();
             toast.success("Post updated");
-            router.push(`/Item/${Item!.id}`)
+            router.push(`/Item/${post!.id}`)
         }
     })
-    if(Item == null) return( <div>Something went wrong</div>)
+    if(post == null) return( <div>Something went wrong</div>)
     const onSubmit = (formData : Item) => {
         void mutate({
-            id: Item.id,
-            product: formData.product || Item.product,
-            price: parseFloat(formData.price) || (Item.price),
-            description: formData.description || Item.description,
-            image: image[0]?.fileUrl || Item.image
+            id: post.id,
+            product: formData.product || post.product,
+            price: parseFloat(formData.price) || post.price,
+            description: formData.description || post.description,
+            image: image[0]?.fileUrl || post.image
         })
     }
 
@@ -60,21 +61,19 @@ const EditItem: NextPage = () => {
     return(
             <form onSubmit={handleSubmit(onSubmit)} className=" flex gap-4 justify-center items-center min-h-screen md:h-auto bg-gradient-to-b  text-white bg-[#55656d] ">
             <div className="flex flex-col gap-4 items-center justify-center ">
-                <h2 className="align-middle text-xl bold ">Edit post: {Item.product}</h2>
+                <h2 className="align-middle text-xl bold ">Edit post: {post.product}</h2>
                 <label>Product name: </label>
-                <input {...register("product", { required: false})} type="text" placeholder={`${Item.product}`} className="input input-bordered w-full max-w-xs"/>
+                <input {...register("product", { required: false})} type="text" placeholder={`${post.product}`} className="input input-bordered w-full max-w-xs"/>
                 <label>Price: </label>
-                <input {...register("price", { required: false })} type="number"   step="0,01" placeholder={`${Item.price}`} className="input input-bordered w-full max-w-xs" />
+                <input {...register("price", { required: false })} type="number"   step="0,01" placeholder={`${post.price}`} className="input input-bordered w-full max-w-xs" />
                 <label>Description: </label>
-                <textarea {...register("description", { required: false, })}  placeholder={`${Item.description || "Description"} `}className="input input-bordered h-20 w-full max-w-xs"/>
+                <textarea {...register("description", { required: false, })}  placeholder={`${post.description || "Description"} `}className="input input-bordered h-20 w-full max-w-xs"/>
                 <label>Image: </label>
                 <UploadDropzone<OurFileRouter>
                     endpoint="strictImageAttachment"
                     onClientUploadComplete={(res) => {
                     if(res){
-                        setImage(res)
-                    // Do something with the response
-                        ("Files: ", res);
+                        setImage(res);
                         toast.success("Upload Completed");}
                     }}
                     onUploadError={(error: Error) => {
@@ -83,7 +82,7 @@ const EditItem: NextPage = () => {
                     }}
                 />
                 <div className="flex justify-center gap-4 w-1/2">
-                    <Link href={`/Item/${Item.id}`}className="btn btn-neutral w-20">Cancel</Link>
+                    <Link href={`/Item/${post.id}`}className="btn btn-neutral w-20">Cancel</Link>
                     <button className="btn btn-accent w-20" type="submit" disabled={loadingItem}>Save</button>
                 </div>
                 <br></br>
